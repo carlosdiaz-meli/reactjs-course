91bca251ed44761a657d316a58f58995634132ba
Carlos Eduardo Díaz Valbuena <carlos.cdiaz@mercadolibre.com.co>
2020-12-10 17:26:30 -0500

    docs updated

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async category => {
 
     const apiKey = 'sqQOHDNaCw1JvcYTbrnetsT7NuYdwFXU';
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&limit=10&q=${ encodeURI( category ) }`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&limit=10&q=${ encodeURI( category ) }`;
 
     const resp   = await fetch(url);
     const { data } = await resp.json(); 
